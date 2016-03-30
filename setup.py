400f91c46a0b2c0499a15379a4f079c578ea2ff7
Diogo Almeida <diogoa@kth.se>
2016-03-30 09:36:58 +0200

    Exposed rospyaction through setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup file to expose python libraries."""
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['behavior_trees'],
+    package_dir={'': 'src'},
+)
+
+setup(**setup_args)
